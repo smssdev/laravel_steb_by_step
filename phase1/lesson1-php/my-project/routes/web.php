b61faefdf0989d8124cba11df3b5c6317672653c
Solayman Salah <slieman2001@hotmail.com>
2025-04-29 15:23:06 +0300

    finish leeson1

--- phase1/lesson1-php/my-project/routes/web.php
+++ phase1/lesson1-php/my-project/routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'مرحباً بك في Laravel!';
+});
+
+Route::get('/about_simple', function () {
+    return 'اسمي: سليمان<br>عمري: 32 سنة<br>هواياتي: البرمجة، القراءة، الرياضة';
+});
+
+Route::get('/about', function () {
+    $name= "Solayman";
+    $age= "32";
+    $hobbies= ['Football', 'Reading', 'WatchingFilms'];
+    // $data=[
+    // 'name'=> $name,
+    // 'age'=> $age,
+    // 'hobbies' => $hobbies
+    // ];
+    // return $data;
+    // return view('about', $data);
+    return view('about', compact('name', 'age', 'hobbies'));
+});
+
+Route::get('/calculator/{num1}/{num2}', function ($num1,$num2) {
+    $sum= $num1 + $num2;
+    // return "مجموع ${num1} و  ${num2} هو : ${sum} ";
+    return "$num1 + $num2 = $sum";
+});
