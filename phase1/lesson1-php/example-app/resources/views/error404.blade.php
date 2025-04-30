c572217af8d633a910290c35425660cef5819d70
Solayman Salah <slieman2001@hotmail.com>
2025-04-30 11:23:42 +0200

    ..

---
+++ phase1/lesson1-php/example-app/resources/views/error404.blade.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+    <h1 style="margin:18% auto;text-align:center;font-size:96px;color:red;">الصفحة غير موجودة</h1>
+</body>
+</html>
--- phase1/lesson1-php/example-app/routes/web.php
+++ phase1/lesson1-php/example-app/routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index']);
 
+Route::any('/any', function () {
+    return 'هذا المسار يستجيب لجميع طرق HTTP';
+});
+
+// الاستجابة لعدة طرق
+Route::match(['get', 'post'], '/match', function () {
+    return 'هذا المسار يستجيب لطلبات GET و POST';
+});
+
+// يجب أن يكون هذا المسار دائماً في نهاية ملف routes/web.php
+Route::fallback(function () {
+    // // يمكنك هنا إرجاع عرض مخصص
+    // return view('error404');
+
+    // // أو إرجاع استجابة مخصصة
+    return response()->view('error404', [], 404);
+    // أو ببساطة إرجاع رسالة
+    // return 'المسار المطلوب غير موجود!';
+});
